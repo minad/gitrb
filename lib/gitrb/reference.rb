1d3796e13fe69ae78821f593899b88ec22f83768
Scott Bronson <brons_git@rinspin.com>
2010-06-06 18:00:22 -0700

    Fix Ruby 1.9.2 problem where a reference always claims to be coercable to an array.
    NoMethodError in 'Gitrb should load log'
    undefined method `to_ary' for 648cb48fa588ca9bd29131e491c657b29fa643a4:Gitrb::Commit
    /home/bronson/gopost/gitrb/lib/gitrb/reference.rb:13:in `method_missing'
    /home/bronson/gopost/gitrb/lib/gitrb/reference.rb:21:in `method_missing'
    /home/bronson/gopost/gitrb/lib/gitrb/commit.rb:8:in `flatten'

--- lib/gitrb/reference.rb
+++ lib/gitrb/reference.rb
@@ -9,7 +9,11 @@ module Gitrb
 
     def method_missing(name, *args, &block)
       if @object
-        instance_eval %{def self.#{name}(*args, &block); @object.send("#{name}", *args, &block); end}
+        unless name == :to_ary || name == :to_str
+          # Ruby 1.9 uses the presence of the to_ary and to_str methods to determine if an object is coercable.
+          # If we create these methods, Ruby will incorrectly think that the object can be converted to an array.
+          instance_eval %{def self.#{name}(*args, &block); @object.send("#{name}", *args, &block); end}
+        end
         @object.send(name, *args, &block)
       elsif name == :type && (mode = @properties['mode'] || @properties[:mode])
         (mode & 040000 == 040000) ? :tree : :blob
